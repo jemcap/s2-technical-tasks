0b34362973f3c26787126ee219b61b2a8e2155b3
JCapito <joshuaecapito22@gmail.com>
2024-10-23 10:00:02 +0100

    Feature: Implemented pseudocode for the initial approach to the problem

---
+++ tasks.js
@@ -0,0 +1,45 @@
+// TASK 1:
+// Write a function / method to loop through a range of numbers, from X to Y
+// Highlight any that are divisible by 3 as "divisible by 3"
+// Highlight any that are divisible by 5 as "divisible by 5"
+// Highlight any that are divisible by both as "divisible by 3 and 5"
+// ----------------------------------------------------------------------------------
+// Code:
+
+// PSEUDOCODE
+// write a function and pass in an array of nums as an argument.
+// declare an array that will hold the numbers that are divisible by X, Y, Z
+// loop through the array
+// if each number in the array is divisible by X, Y, Z, push the numbers to the array
+// return each array
+
+// ----------------------------------------------------------------------------------
+// ==================================================================================
+
+// TASK 2:
+// Write an function / method to accept a multi-dimensional JSON array, and produce an equivalent list grouped by any valid field
+// Use the following JSON array:
+// [
+//   {car_make: "bmw", car_model: "m2", transmission: "automatic"},
+//   {car_make: "audi", car_model: "A3", transmission: "automatic"},
+//   {car_make: "ford", car_model: "focus", transmission: "manual"},
+//   {car_make: "audi", car_model: "quatro", transmission: "manual"},
+
+//   {car_make: "bmw", car_model: "330i", transmission: "manual"},
+//   {car_make: "nissan", car_model: "leaf", transmission: "automatic"}
+// ]
+
+// If grouping by car_make, for example, the output format should be:
+// [
+//   {car_make: "bmw", items: [{car_model: "m2", transmission: "automatic"}, {car_model: "330i", transmission: "manual"}]},
+//   {car_make: "audi", items: [{car_model: "A3", transmission: "automatic"}, {car_model: "quatro", transmission: "manual"}]},
+
+//   {car_make: "ford", items: [{car_model: "focus", transmission: "manual"}]},
+
+//   {car_make: "nissan", items: [{car_model: "leaf", transmission: "automatic"}]}
+
+// ]
+// ----------------------------------------------------------------------------------
+// Code:
+
+// ----------------------------------------------------------------------------------
